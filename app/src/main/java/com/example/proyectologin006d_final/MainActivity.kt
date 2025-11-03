79ed8e264ae66162b652952744a9c9e2fba98dd0
Koveb-dev <ce.flores@duocuc.cl>
2025-11-02 23:51:04 -0300

    cambio de version de 8.13 a 8.12.1 en agp gradle\libs.version.toml

--- app/src/main/java/com/example/proyectologin006d_final/MainActivity.kt
+++ app/src/main/java/com/example/proyectologin006d_final/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.proyectologin006d_final.navigation.AppNav
-import com.example.proyectologin006d_final.navigation.AppNav
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
